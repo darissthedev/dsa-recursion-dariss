d066f2555b2635a8e81ea82fcf87ed5706a77240
Dariss Harris <darissharris@gmail.com>
2020-11-30 14:01:37 -0800

    'maze 2 exercise'

--- dsa-recursion-dariss.js
+++ dsa-recursion-dariss.js
@@ -126,7 +126,49 @@ console.log(maze(0, 0, mySmallMaze));
 console.log(maze(0, 0, myLargeMaze));
 
 //Find All the ways out of the maze
-
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+  ];
+  let bigMaze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+  ];
+  function copyMaze(maze) {
+    const copy = [];
+    maze.forEach(row => {
+      copy.push([...row]);
+    });
+    return copy;
+  }
+  function pathFinder(maze, x = 0, y = 0, currPath = '', paths = []) {
+    const currRow = maze[y];
+    const directions = {
+      'R': (x, y) => [x + 1, y],
+      'D': (x, y) => [x, y + 1],
+      'L': (x, y) => [x - 1, y],
+      'U': (x, y) => [x, y - 1]
+    };
+    if(!currRow) return false;
+    const currSpace = currRow[x];
+    if(!currSpace) return false;
+    if(currSpace === '*') return false;
+    if(currSpace === 'e') {
+      console.log('Path to the exit:', currPath);
+      paths.push(currPath);
+      return true;
+    }
+    currRow[x] = '*';
+    for (const [dir, func] of Object.entries(directions)) {
+      pathFinder(copyMaze(maze), ...func(x, y), currPath + dir, paths);
+    }
+    return paths;
+  }
+  pathFinder(bigMaze);
 //Anagrams
 
 //Organization Chart
